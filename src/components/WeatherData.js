6d0c5734eb10a552a44132c7fd06b3a647b4d747
david.torres@visionaria.com <otrebla.120988>
2018-11-12 19:51:46 -0600

    Cambios

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -2,11 +2,11 @@ import React from 'react';
 import WheatherExtraInfo from './WeatherExtraInfo';
 import WheatherTemperature from './WheatherTemperature';
 
-const WheatherData=() =>(
-<div>
-    <WheatherExtraInfo humidity={80} wind={"10 m/s"}/>
-    <WheatherTemperature temperature={30}/>
-</div>
+const WheatherData = () => (
+    <div>
+        <WheatherExtraInfo humidity={80} wind={"10 m/s"}/>
+        <WheatherTemperature temperature={30} weatherState={''}/>
+    </div>
 
 
 );
--- src/components/WheatherTemperature.js
+++ src/components/WheatherTemperature.js
@@ -1,5 +1,26 @@
 import React from 'react';
-const WheatherTemperature=({temperature})=>(
-    <div><span>{temperature + "°"}</span></div>
+import WeatherIcons from 'react-weathericons';
+const icons={
+    cloud:"cloud",
+    cloudy:"cloudy",
+    rain:"rain",
+    sunny:"day-sunny",
+    fog:"day-fog",
+    snow:"snow",
+    windy:"windy",
+};
+const getWeatherIcon=weatherState=>{
+    const icon= icons[weatherState];
+    if(icon)
+    return <WeatherIcons name={icon}  size="2x" />
+    else
+        return <WeatherIcons name={"day-sunny"}  size="2x" />
+}
+const WheatherTemperature=({temperature, weatherState})=>(
+    <div>
+        {getWeatherIcon(weatherState)}
+
+        <span>{" "+temperature + "C°"}</span>
+    </div>
 );
 export default WheatherTemperature;
